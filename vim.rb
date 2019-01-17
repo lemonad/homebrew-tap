d6e442589a3cd33e1e485d30dca559a2d0e58d42
Jonas Nockert <jonasnockert@gmail.com>
2019-01-17 13:28:18 +0100

    vim with --with-client-server option

---
+++ vim.rb
@@ -0,0 +1,87 @@
+class Vim < Formula
+  desc "Vi 'workalike' with many additional features"
+  homepage "https://www.vim.org/"
+  # vim should only be updated every 50 releases on multiples of 50
+  url "https://github.com/vim/vim/archive/v8.1.0750.tar.gz"
+  sha256 "3ae7deca957d48a61b72bd34c6ee9869583a4978bc27367b023a1cae506899be"
+  head "https://github.com/vim/vim.git"
+
+  bottle do
+    cellar :any
+    sha256 "761bc4465135ba23ac3c64a418bb3a07e85ca2f36a350c2b8e64fe818043ff54" => :mojave
+    sha256 "3e73d86d83522d1b292ac2bda631a3c41fd99dae76ce236ca8ab8a968b5c05c9" => :high_sierra
+    sha256 "809c0dc03514e82c2b45fb94e854bb0e3ee6b0939e845382a2e6a645b8619526" => :sierra
+  end
+
+  option "with-client-server", "Enable client/server mode"
+
+  depends_on "gettext"
+  depends_on "lua"
+  depends_on "perl"
+  depends_on "python"
+  depends_on "ruby"
+  depends_on :x11 if build.with? "client-server"
+
+  conflicts_with "ex-vi",
+    :because => "vim and ex-vi both install bin/ex and bin/view"
+
+  conflicts_with "macvim",
+    :because => "vim and macvim both install vi* binaries"
+
+  def install
+    ENV.prepend_path "PATH", Formula["python"].opt_libexec/"bin"
+
+    # https://github.com/Homebrew/homebrew-core/pull/1046
+    ENV.delete("SDKROOT")
+
+    # vim doesn't require any Python package, unset PYTHONPATH.
+    ENV.delete("PYTHONPATH")
+
+    if build.with? "client-server"
+      opts << "--with-x"
+    else
+      opts << "--without-x"
+    end
+
+    # We specify HOMEBREW_PREFIX as the prefix to make vim look in the
+    # the right place (HOMEBREW_PREFIX/share/vim/{vimrc,vimfiles}) for
+    # system vimscript files. We specify the normal installation prefix
+    # when calling "make install".
+    # Homebrew will use the first suitable Perl & Ruby in your PATH if you
+    # build from source. Please don't attempt to hardcode either.
+    system "./configure", "--prefix=#{HOMEBREW_PREFIX}",
+                          "--mandir=#{man}",
+                          "--enable-multibyte",
+                          "--with-tlib=ncurses",
+                          "--enable-cscope",
+                          "--enable-terminal",
+                          "--with-compiledby=Homebrew",
+                          *opts
+                          "--enable-perlinterp",
+                          "--enable-rubyinterp",
+                          "--enable-python3interp",
+                          "--enable-gui=no",
+                          "--without-x",
+                          "--enable-luainterp",
+                          "--with-lua-prefix=#{Formula["lua"].opt_prefix}"
+    system "make"
+    # Parallel install could miss some symlinks
+    # https://github.com/vim/vim/issues/1031
+    ENV.deparallelize
+    # If stripping the binaries is enabled, vim will segfault with
+    # statically-linked interpreters like ruby
+    # https://github.com/vim/vim/issues/114
+    system "make", "install", "prefix=#{prefix}", "STRIP=#{which "true"}"
+    bin.install_symlink "vim" => "vi"
+  end
+
+  test do
+    (testpath/"commands.vim").write <<~EOS
+      :python3 import vim; vim.current.buffer[0] = 'hello python3'
+      :wq
+    EOS
+    system bin/"vim", "-T", "dumb", "-s", "commands.vim", "test.txt"
+    assert_equal "hello python3", File.read("test.txt").chomp
+    assert_match "+gettext", shell_output("#{bin}/vim --version")
+  end
+end
